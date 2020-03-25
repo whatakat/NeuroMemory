894cb4ecc2c037421dc0e2bb3f7a4c28be9d1864
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-25 22:54:00 +0300

    initialisation init

--- app/src/main/java/com/bankmtk/neuromemory/data/Repository.kt
+++ app/src/main/java/com/bankmtk/neuromemory/data/Repository.kt
@@ -3,13 +3,11 @@ package com.bankmtk.neuromemory.data
 import com.bankmtk.neuromemory.data.model.Sticker
 
 object Repository {
-    private val stickers: List<Sticker>
-    init {
-        stickers = listOf(
-            Sticker("Простите ребята, кто не успел - тот опоздал",
-            "Sorry fellas waste not wan't not",0xfff06292.toInt())
-        )
-    }
+    private val stickers: List<Sticker> = listOf(
+        Sticker("Простите ребята, кто не успел - тот опоздал",
+        "Sorry fellas waste not wan't not",0xfff06292.toInt())
+    )
+
     fun getStickers():List<Sticker>{
         return stickers
     }
