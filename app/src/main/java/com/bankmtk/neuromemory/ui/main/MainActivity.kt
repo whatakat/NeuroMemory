20e544294092f91de140247ef9dc1d11fae2c7d6
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-27 23:25:15 +0300

    MainActivity & correct layout

--- app/src/main/java/com/bankmtk/neuromemory/ui/main/MainActivity.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/main/MainActivity.kt
@@ -2,12 +2,27 @@ package com.bankmtk.neuromemory.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.bankmtk.neuromemory.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MainViewModel
+    lateinit var adapter: MainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        adapter = MainAdapter()
+        myRecycler.adapter = adapter
+
+        viewModel.viewState().observe(this,Observer<MainViewState>{
+            t ->t?.let{adapter.stickers = it.stickers}
+        })
     }
 }
