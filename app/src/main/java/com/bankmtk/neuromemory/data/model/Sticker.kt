14f7ad9e718fd724a1e1a0b9595a6179c590935c
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-29 23:14:32 +0300

    correct Sticker

--- app/src/main/java/com/bankmtk/neuromemory/data/model/Sticker.kt
+++ app/src/main/java/com/bankmtk/neuromemory/data/model/Sticker.kt
@@ -1,3 +1,35 @@
 package com.bankmtk.neuromemory.data.model
 
-class Sticker (val langOne: String, val langTwo: String, val color: Int)
+import android.os.Parcelable
+import java.util.*
+
+@Parscelize
+data class Sticker (val id: String,
+                    val langOne: String,
+                    val langTwo: String,
+                    val color: Color = Color.WHITE,
+                    val lastChanged: Date = Date()):Parcelable{
+    override fun equals(other: Any?): Boolean {
+       if (this==other) return true
+        if (JavaClass != other?.JavaClass) return false
+
+        other as Sticker
+        if (id != other.id) return  false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
+
+enum class Color{
+    WHITE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    RED,
+    VIOLET,
+    PINK
+}
