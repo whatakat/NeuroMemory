b216fce496712c2846f8c308456d8c5fd0e44c09
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-29 23:39:06 +0300

    correct Repository

--- app/src/main/java/com/bankmtk/neuromemory/data/Repository.kt
+++ app/src/main/java/com/bankmtk/neuromemory/data/Repository.kt
@@ -1,17 +1,27 @@
 package com.bankmtk.neuromemory.data
 
+import com.bankmtk.neuromemory.data.model.Color
 import com.bankmtk.neuromemory.data.model.Sticker
+import java.util.*
 
 object Repository {
-    private val stickers: List<Sticker> = listOf(
-        Sticker("Простите ребята, кто не успел - тот опоздал",
-        "Sorry fellas waste not wan't not",0xfff06292.toInt()),
-                Sticker("Простите ребята, кто не успел - тот опоздал",
-        "Sorry fellas waste not wan't not",0xfff06292.toInt()),
-        Sticker("Простите ребята, кто не успел - тот опоздал",
-            "Sorry fellas waste not wan't not",0xfff06292.toInt())
-
-    )
+    private val stickers: MutableList<Sticker> = mutableListOf(
+        Sticker(id = UUID.randomUUID().toString(),
+            langOne = "Простите ребята, кто не успел - тот опоздал",
+            langTwo = "Sorry fellas waste not wan't not",
+            color = Color.WHITE),
+        Sticker(id = UUID.randomUUID().toString(),
+            langOne = "Что ты думаешь на счет этого",
+            langTwo = "What do you mind about this",
+            color = Color.WHITE),
+        Sticker(id = UUID.randomUUID().toString(),
+            langOne = "Ты шутишь?",
+            langTwo = "Are you kidding?",
+            color = Color.WHITE),
+        Sticker(id = UUID.randomUUID().toString(),
+            langOne = "Можешь дать мне подсказку",
+            langTwo = "can you give me a clue?",
+            color = Color.WHITE))
 
     fun getStickers():List<Sticker>{
         return stickers
