6f21a3a855772bafbe0f46fc5ea46f7598594133
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-03-30 23:16:26 +0300

    correct Repository

--- app/src/main/java/com/bankmtk/neuromemory/data/Repository.kt
+++ app/src/main/java/com/bankmtk/neuromemory/data/Repository.kt
@@ -9,19 +9,23 @@ object Repository {
         Sticker(id = UUID.randomUUID().toString(),
             langOne = "Простите ребята, кто не успел - тот опоздал",
             langTwo = "Sorry fellas waste not wan't not",
-            color = Color.WHITE),
+            color = Color.WHITE,
+        title = "Sample"),
         Sticker(id = UUID.randomUUID().toString(),
             langOne = "Что ты думаешь на счет этого",
             langTwo = "What do you mind about this",
-            color = Color.WHITE),
+            color = Color.WHITE,
+        title = "Sample"),
         Sticker(id = UUID.randomUUID().toString(),
             langOne = "Ты шутишь?",
             langTwo = "Are you kidding?",
-            color = Color.WHITE),
+            color = Color.WHITE,
+        title = "sample"),
         Sticker(id = UUID.randomUUID().toString(),
             langOne = "Можешь дать мне подсказку",
             langTwo = "can you give me a clue?",
-            color = Color.WHITE))
+            color = Color.WHITE,
+        title = "Sample"))
 
     fun getStickers():List<Sticker>{
         return stickers
