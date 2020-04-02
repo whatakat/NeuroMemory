e55b9f9ed6fad57751f61f290f0f9eb9286e3dbd
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-02 14:53:11 +0300

    add BaseViewModel

---
+++ app/src/main/java/com/bankmtk/neuromemory/ui/base/BaseViewModel.kt
@@ -0,0 +1,10 @@
+package com.bankmtk.neuromemory.ui.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel<T, S : BaseViewState<T>>: ViewModel() {
+    open val viewStateLiveData = MutableLiveData<S>()
+    open fun getViewState(): LiveData<S> = viewStateLiveData
+}
