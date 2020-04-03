0d7f7380183a427def98d76f38205d968afebb41
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-03 22:13:52 +0300

    correct MainViewState

--- app/src/main/java/com/bankmtk/neuromemory/ui/main/MainViewState.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/main/MainViewState.kt
@@ -1,10 +1,7 @@
 package com.bankmtk.neuromemory.ui.main
 
 import com.bankmtk.neuromemory.data.model.Sticker
+import com.bankmtk.neuromemory.ui.base.BaseViewState
 
-class MainViewState (val stickers: List<Sticker>) {
-    fun copy(stickers: List<Sticker>): MainViewState? {
-        return null
-    }
-
-}
+class MainViewState (stickers: List<Sticker>? = null, error: Throwable? = null):
+BaseViewState<List<Sticker>?>(stickers,error)
