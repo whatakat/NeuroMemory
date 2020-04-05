2653f11198565d408dc75c1ebcf9b66a6e22014b
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-05 22:18:06 +0300

    refactor Extensions

--- app/src/main/java/com/bankmtk/neuromemory/extentions/Constants.kt
+++
@@ -1,3 +0,0 @@
-package com.bankmtk.neuromemory.extentions
-
-const val DATE_TIME_FORMAT = "dd.MMM.yy HH:mm"
---
+++ app/src/main/java/com/bankmtk/neuromemory/extentions/Extensions.kt
@@ -0,0 +1,24 @@
+package com.bankmtk.neuromemory.extentions
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import com.bankmtk.neuromemory.R
+import com.bankmtk.neuromemory.data.model.Color
+import java.text.SimpleDateFormat
+import java.util.*
+
+const val DATE_TIME_FORMAT = "dd.MMM.yy HH:mm"
+
+fun Date.format(): String =
+    SimpleDateFormat(DATE_TIME_FORMAT, Locale.getDefault())
+        .format(this)
+fun Color.getColorInt(context: Context): Int =
+    ContextCompat.getColor(context, when(this){
+        Color.WHITE -> R.color.white
+        Color.GREEN -> R.color.green
+        Color.YELLOW -> R.color.yellow
+        Color.RED -> R.color.red
+        Color.PINK -> R.color.pink
+        Color.BLUE -> R.color.blue
+        Color.VIOLET -> R.color.violet
+    })
