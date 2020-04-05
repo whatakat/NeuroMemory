2be2467ba3be7df38a8c0d56124cfd02f60048ee
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-05 22:44:24 +0300

    refactor BaseActivity

--- app/src/main/java/com/bankmtk/neuromemory/ui/base/BaseActivity.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/base/BaseActivity.kt
@@ -1,14 +1,13 @@
 package com.bankmtk.neuromemory.ui.base
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.bankmtk.neuromemory.R
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
-abstract class BaseActivity <T, S : BaseViewState<T>> : AppCompatActivity(){
+abstract class BaseActivity<T, S : BaseViewState<T>> : AppCompatActivity() {
 
     abstract val viewModel: BaseViewModel<T, S>
     abstract val layoutRes: Int
@@ -16,24 +15,24 @@ abstract class BaseActivity <T, S : BaseViewState<T>> : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layoutRes)
-        viewModel.getViewState().observe(this, object : Observer<S>{
-            override fun onChanged(t:S?){
-                if (t == null) return
-                if (t.data != null) renderData(t.data!!)
-                if (t.error != null)renderError(t.error)
+        viewModel.getViewState().observe(this, Observer<S> { t ->
+            t?.apply {
+                data?.let { renderData(it) }
+                error?.let { renderError(it) }
             }
         })
     }
-    protected fun renderError(error: Throwable){
-        if (error.message != null) showError(error.message!!)
+
+    protected fun renderError(error: Throwable) {
+        error.message?.let { showError(it) }
     }
+
     abstract fun renderData(data: T)
 
-    protected fun showError(error: String){
-        val snackbar = Snackbar.make(myRecycler,error,
-        Snackbar.LENGTH_INDEFINITE)
-        snackbar.setAction(R.string.ok_bth_title,View.OnClickListener {
-            snackbar.dismiss()})
-        snackbar.show()
+    protected fun showError(error: String) {
+        Snackbar.make(myRecycler, error, Snackbar.LENGTH_INDEFINITE).apply {
+            setAction(R.string.ok_bth_title) { dismiss() }
+            show()
+        }
     }
 }
