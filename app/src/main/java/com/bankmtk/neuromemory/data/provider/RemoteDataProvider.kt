a2017a9d9dedba1a6795f866209e960af7463305
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-07 22:34:43 +0300

    add getCurrentUser

--- app/src/main/java/com/bankmtk/neuromemory/data/provider/RemoteDataProvider.kt
+++ app/src/main/java/com/bankmtk/neuromemory/data/provider/RemoteDataProvider.kt
@@ -3,9 +3,11 @@ package com.bankmtk.neuromemory.data.provider
 import androidx.lifecycle.LiveData
 import com.bankmtk.neuromemory.data.model.Sticker
 import com.bankmtk.neuromemory.data.model.StickerResult
+import com.bankmtk.neuromemory.data.model.User
 
 interface RemoteDataProvider {
     fun subscribeToAllStickers(): LiveData<StickerResult>
     fun getStickerById(id: String): LiveData<StickerResult>
     fun saveSticker(sticker: Sticker): LiveData<StickerResult>
+    fun getCurrentUser() : LiveData<User?>
 }
