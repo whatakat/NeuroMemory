79c5e8c019941fb1f9c748e2292de2e913afa09f
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-08 21:18:51 +0300

    add class SplashViewState

---
+++ app/src/main/java/com/bankmtk/neuromemory/ui/splash/SplashViewState.kt
@@ -0,0 +1,6 @@
+package com.bankmtk.neuromemory.ui.splash
+
+import com.bankmtk.neuromemory.ui.base.BaseViewState
+
+class SplashViewState(isAuth: Boolean? = null, error: Throwable? = null):
+BaseViewState<Boolean?>(isAuth, error)
