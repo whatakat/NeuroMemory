f3b41f57bdbe4d017800e005c0f7c13f1e610b0b
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-08 21:23:33 +0300

    add class SplashViewModel

---
+++ app/src/main/java/com/bankmtk/neuromemory/ui/splash/SplashViewModel.kt
@@ -0,0 +1,18 @@
+package com.bankmtk.neuromemory.ui.splash
+
+import com.bankmtk.neuromemory.data.Repository
+import com.bankmtk.neuromemory.data.errors.NoAuthException
+import com.bankmtk.neuromemory.ui.base.BaseViewModel
+
+class SplashViewModel(private val repository: Repository = Repository):
+BaseViewModel<Boolean?, SplashViewState>(){
+    fun requestUser(){
+        repository.getCurrentUser().observeForever{
+            viewStateLiveData.value = if (it != null){
+                SplashViewState(isAuth = true)
+            }else{
+                SplashViewState(error = NoAuthException())
+            }
+        }
+    }
+}
