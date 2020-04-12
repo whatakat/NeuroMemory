cfa686e0f77250c46a8fa100fde842484a25bbea
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-12 23:10:16 +0300

    refactor StickerViewState

--- app/src/main/java/com/bankmtk/neuromemory/ui/sticker/StickerViewState.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/sticker/StickerViewState.kt
@@ -3,5 +3,7 @@ package com.bankmtk.neuromemory.ui.sticker
 import com.bankmtk.neuromemory.data.model.Sticker
 import com.bankmtk.neuromemory.ui.base.BaseViewState
 
-class StickerViewState (sticker: Sticker? = null,
-                        error: Throwable? = null): BaseViewState<Sticker?>(sticker,error)
+class StickerViewState (data: Data = Data(),
+                        error: Throwable? = null): BaseViewState<StickerViewState.Data>(data,error){
+    data class Data(val isDeleted: Boolean = false, val sticker: Sticker? = null)
+}
