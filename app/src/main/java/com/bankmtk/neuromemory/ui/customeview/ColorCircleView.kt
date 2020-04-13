1e89303dd8dc6aae1b78d29edce550c386001e43
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-13 22:21:24 +0300

    add class ColorCircleView

---
+++ app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorCircleView.kt
@@ -0,0 +1,10 @@
+package com.bankmtk.neuromemory.ui.customeview
+
+import androidx.annotation.Dimension
+import androidx.annotation.Dimension.DP
+
+@Dimension(unit = DP) private const val defRadiusDp = 16
+@Dimension(unit = DP) private const val defStrokeWidthDp = 1
+
+class ColorCircleView {
+}
