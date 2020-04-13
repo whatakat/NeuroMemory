c1f491ac60e371b08a0f27c51cdb168539e96868
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-13 22:25:17 +0300

    add constructor

--- app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorCircleView.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorCircleView.kt
@@ -1,10 +1,14 @@
 package com.bankmtk.neuromemory.ui.customeview
 
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
 import androidx.annotation.Dimension
 import androidx.annotation.Dimension.DP
 
 @Dimension(unit = DP) private const val defRadiusDp = 16
 @Dimension(unit = DP) private const val defStrokeWidthDp = 1
 
-class ColorCircleView {
+class ColorCircleView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null,
+defStyleAttr: Int = 0): View(context, attrs, defStyleAttr){
 }
