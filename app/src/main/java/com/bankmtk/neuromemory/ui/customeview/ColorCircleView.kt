b7d08a9738cbd141285a2e2f8229567df9082cbf
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-14 12:40:20 +0300

    add fillColors

--- app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorCircleView.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorCircleView.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import androidx.annotation.ColorRes
 import androidx.annotation.Dimension
 import androidx.annotation.Dimension.DP
 import androidx.annotation.Dimension.PX
+import androidx.core.content.ContextCompat
+import com.bankmtk.neuromemory.R
 import org.jetbrains.anko.dip
 
 @Dimension(unit = DP) private const val defRadiusDp = 16
@@ -24,4 +27,10 @@ defStyleAttr: Int = 0): View(context, attrs, defStyleAttr){
     private var center: Pair<Float, Float> = 0f to 0f
 
     @Dimension(unit = PX) var radius: Float = dip(defRadiusDp).toFloat()
+
+    @ColorRes var fillColorRes: Int = R.color.white
+    set(value) {
+        field = value
+        fillPaint.color = ContextCompat.getColor(context,value)
+    }
 }
