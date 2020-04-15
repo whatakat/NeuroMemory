55e697ff4657e114cd79d64372a88f15b6963c37
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-15 19:14:54 +0300

    isOpen

--- app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorPickerView.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorPickerView.kt
@@ -1,11 +1,18 @@
 package com.bankmtk.neuromemory.ui.customeview
 
+import android.widget.LinearLayout
 import androidx.annotation.Dimension
 import androidx.annotation.Dimension.DP
+import com.bankmtk.neuromemory.data.model.Color
 
 private const val PALETTE_ANIMATION_DURATION = 150L
 private const val HEIGHT = "height"
 private const val SCALE = "scale"
 @Dimension(unit = DP) private const val COLOR_VIEW_PADDING = 8
-class ColorPickerView {
+class ColorPickerView: LinearLayout {
+    var onColorClickListener: (color:Color) ->Unit = {}
+
+    val isOpen: Boolean
+    get() = measuredHeight>0
+
 }
