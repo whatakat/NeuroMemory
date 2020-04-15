74e870a1e25ecb26e88d7bcf41f84957e4b91640
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-15 19:23:41 +0300

    add updateListener

--- app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorPickerView.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorPickerView.kt
@@ -23,7 +23,24 @@ class ColorPickerView: LinearLayout {
             duration = PALETTE_ANIMATION_DURATION
             addUpdateListener(updateListener)
         }
-
     }
+    private val updateListener by lazy {
+        ValueAnimator.AnimatorUpdateListener { animator ->
+            layoutParams.apply {
+                height = animator.getAnimatedValue(HEIGHT) as Int
+            }.let {
+                layoutParams = it
+            }
+            val scaleFactor = animator.getAnimatedValue(SCALE) as Float
+            for (i in 0 until childCount){
+                getChildAt(i).apply {
+                    scaleX = scaleFactor
+                    scaleY = scaleFactor
+                    alpha = scaleFactor
+                }
+            }
+        }
+    }
+
 
 }
