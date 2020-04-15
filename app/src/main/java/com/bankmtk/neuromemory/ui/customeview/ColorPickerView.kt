5112e28cdef7723c7f91be0cbd79d3dc829df02e
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-15 19:10:54 +0300

    add constants

--- app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorPickerView.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorPickerView.kt
@@ -1,4 +1,6 @@
 package com.bankmtk.neuromemory.ui.customeview
-
+private const val PALETTE_ANIMATION_DURATION = 150L
+private const val HEIGHT = "height"
+private const val SCALE = "scale"
 class ColorPickerView {
 }
