025cc8d7fd825d0b232e410f6a1af466b16620f7
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-15 19:12:26 +0300

    add constant Dimension

--- app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorPickerView.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/customeview/ColorPickerView.kt
@@ -1,6 +1,11 @@
 package com.bankmtk.neuromemory.ui.customeview
+
+import androidx.annotation.Dimension
+import androidx.annotation.Dimension.DP
+
 private const val PALETTE_ANIMATION_DURATION = 150L
 private const val HEIGHT = "height"
 private const val SCALE = "scale"
+@Dimension(unit = DP) private const val COLOR_VIEW_PADDING = 8
 class ColorPickerView {
 }
