9ff017eac4619e9bc756f77607432b7246f075aa
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-16 14:42:38 +0300

    correct Repository

--- app/src/main/java/com/bankmtk/neuromemory/data/Repository.kt
+++ app/src/main/java/com/bankmtk/neuromemory/data/Repository.kt
@@ -4,9 +4,9 @@ import com.bankmtk.neuromemory.data.model.Sticker
 import com.bankmtk.neuromemory.data.provider.FireStoreProvider
 import com.bankmtk.neuromemory.data.provider.RemoteDataProvider
 
-object Repository {
+class Repository(private val remoteProvider: RemoteDataProvider) {
 
-    private val remoteProvider: RemoteDataProvider = FireStoreProvider()
+    //private val remoteProvider: RemoteDataProvider = FireStoreProvider()
 
     fun getStickers() = remoteProvider.subscribeToAllStickers()
     fun saveSticker(sticker: Sticker)= remoteProvider.saveSticker(sticker)
