773752612daa9f136c0266bec31a20b8e4a3741f
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-17 23:33:36 +0300

    add MainViewModelTest

---
+++ app/src/test/java/com/bankmtk/neuromemory/ui/MainViewModelTest.kt
@@ -0,0 +1,9 @@
+package com.bankmtk.neuromemory.ui
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+
+class MainViewModelTest {
+    @get: Rule
+    val taskExecutorRule = InstantTaskExecutorRule()
+}
