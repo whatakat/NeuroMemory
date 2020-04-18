d480e482b963f6cf5c10edf27dfa00ab341b2481
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-18 14:28:25 +0300

    add implementation

--- app/src/test/java/com/bankmtk/neuromemory/ui/MainViewModelTest.kt
+++ app/src/test/java/com/bankmtk/neuromemory/ui/MainViewModelTest.kt
@@ -1,9 +1,16 @@
 package com.bankmtk.neuromemory.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.bankmtk.neuromemory.data.Repository
+import com.nhaarman.mockitokotlin2.mock
+
+import io.mockk.MockK
+
 import org.junit.Rule
 
 class MainViewModelTest {
     @get: Rule
     val taskExecutorRule = InstantTaskExecutorRule()
+
+    private val mockRepositoty: Repository = mock<Repository>()
 }
