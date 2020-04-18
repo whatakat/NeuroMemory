985e25cea587e3b12a6d328bc274ca0ccf3c9fa2
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-18 15:47:50 +0300

    add Role

--- app/src/test/java/com/bankmtk/neuromemory/data/FireStoreProviderTest.kt
+++ app/src/test/java/com/bankmtk/neuromemory/data/FireStoreProviderTest.kt
@@ -1,4 +1,10 @@
 package com.bankmtk.neuromemory.data
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+
 class FireStoreProviderTest {
+    @get: Rule
+    val taskExecutorRule = InstantTaskExecutorRule()
+
 }
