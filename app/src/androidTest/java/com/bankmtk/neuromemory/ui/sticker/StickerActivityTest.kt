3ecc9b539696841d32185f6dbd1416615f81db0d
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-21 13:02:11 +0300

    add androidTestImplementation mock

--- app/src/androidTest/java/com/bankmtk/neuromemory/ui/sticker/StickerActivityTest.kt
+++ app/src/androidTest/java/com/bankmtk/neuromemory/ui/sticker/StickerActivityTest.kt
@@ -1,9 +1,12 @@
 package com.bankmtk.neuromemory.ui.sticker
 
 import org.junit.Rule
-import android.support.test.rule.ActivityTestRule
+import androidx.test.rule.ActivityTestRule
+import io.mockk.spyk
 
 class StickerActivityTest{
     @get: Rule
     val activityTestRule = ActivityTestRule(StickerActivity::class.java, true, false)
+
+    private val viewModel: StickerViewModel = spyk()
 }
