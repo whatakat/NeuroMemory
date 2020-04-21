ac206d7d6f6dae71eaa33ecaf87dec1c7b12a0cf
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-21 13:04:10 +0300

    correct viewModel

--- app/src/androidTest/java/com/bankmtk/neuromemory/ui/sticker/StickerActivityTest.kt
+++ app/src/androidTest/java/com/bankmtk/neuromemory/ui/sticker/StickerActivityTest.kt
@@ -2,11 +2,12 @@ package com.bankmtk.neuromemory.ui.sticker
 
 import org.junit.Rule
 import androidx.test.rule.ActivityTestRule
+import io.mockk.mockk
 import io.mockk.spyk
 
 class StickerActivityTest{
     @get: Rule
     val activityTestRule = ActivityTestRule(StickerActivity::class.java, true, false)
 
-    private val viewModel: StickerViewModel = spyk()
+    private val viewModel: StickerViewModel = spyk(StickerViewModel(mockk()))
 }
