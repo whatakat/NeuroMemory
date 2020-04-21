4f26a7e04b247e02b54c107acc605dafef90d2d0
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-21 14:26:36 +0300

    add fun should_show_color_picker()

--- app/src/androidTest/java/com/bankmtk/neuromemory/ui/sticker/StickerActivityTest.kt
+++ app/src/androidTest/java/com/bankmtk/neuromemory/ui/sticker/StickerActivityTest.kt
@@ -2,14 +2,22 @@ package com.bankmtk.neuromemory.ui.sticker
 
 import android.content.Intent
 import androidx.lifecycle.MutableLiveData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import org.junit.Rule
 import androidx.test.rule.ActivityTestRule
+import com.bankmtk.neuromemory.R
 import com.bankmtk.neuromemory.data.model.Sticker
 import io.mockk.*
+import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 import org.koin.standalone.StandAloneContext.loadKoinModules
+import java.util.regex.Pattern.matches
 
 
 class StickerActivityTest{
@@ -39,7 +47,12 @@ class StickerActivityTest{
 
     private val testSticker = Sticker("333","title","langOne","langTwo")
 
+    @Test
+    fun should_show_color_picker(){
+        onView(withId(R.id.palette)).perform(click())
 
+        onView(withId(R.id.colorPicker)).check(matches(isCompletelyDisplayed()))
+    }
 
 
 
