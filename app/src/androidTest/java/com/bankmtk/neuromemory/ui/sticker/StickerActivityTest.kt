cc4ee49a73156d30d096184e8273ec8d05292264
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-21 14:02:51 +0300

    correct fun setUp

--- app/src/androidTest/java/com/bankmtk/neuromemory/ui/sticker/StickerActivityTest.kt
+++ app/src/androidTest/java/com/bankmtk/neuromemory/ui/sticker/StickerActivityTest.kt
@@ -1,10 +1,15 @@
 package com.bankmtk.neuromemory.ui.sticker
 
+import android.content.Intent
 import androidx.lifecycle.MutableLiveData
 import org.junit.Rule
 import androidx.test.rule.ActivityTestRule
 import com.bankmtk.neuromemory.data.model.Sticker
 import io.mockk.*
+import org.junit.Before
+import org.koin.android.viewmodel.ext.koin.viewModel
+import org.koin.dsl.module.module
+import org.koin.standalone.StandAloneContext.loadKoinModules
 
 
 class StickerActivityTest{
@@ -15,14 +20,25 @@ class StickerActivityTest{
 
     private val viewStateLiveData = MutableLiveData<StickerViewState>()
 
+    @Before
     fun setUp(){
+        loadKoinModules(listOf(
+            module { viewModel{viewModel} }
+        ))
         every { viewModel.getViewState() } returns viewStateLiveData
         every { viewModel.loadSticker(any()) } just  runs
         every { viewModel.saveChanges(any()) } just runs
         every { viewModel.deleteSticker() } just runs
+
+        Intent().apply {
+            putExtra(StickerActivity::class.java.name + "extra.STICKER_ID", testSticker.id)
+        }.let {
+            activityTestRule.launchActivity(it)
+        }
     }
 
     private val testSticker = Sticker("333","title","langOne","langTwo")
+
 
 
 
