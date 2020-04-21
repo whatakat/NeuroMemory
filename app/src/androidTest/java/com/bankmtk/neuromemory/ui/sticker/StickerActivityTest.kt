3eb5b85dc3f9d7b967f9ffa69c2b166667701db2
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-21 13:49:58 +0300

    add viewStateLiveData

--- app/src/androidTest/java/com/bankmtk/neuromemory/ui/sticker/StickerActivityTest.kt
+++ app/src/androidTest/java/com/bankmtk/neuromemory/ui/sticker/StickerActivityTest.kt
@@ -1,13 +1,20 @@
 package com.bankmtk.neuromemory.ui.sticker
 
+import androidx.lifecycle.MutableLiveData
 import org.junit.Rule
 import androidx.test.rule.ActivityTestRule
 import io.mockk.mockk
 import io.mockk.spyk
+
 
 class StickerActivityTest{
     @get: Rule
     val activityTestRule = ActivityTestRule(StickerActivity::class.java, true, false)
 
     private val viewModel: StickerViewModel = spyk(StickerViewModel(mockk()))
+
+    private val viewStateLiveData = MutableLiveData<StickerViewState>()
+
+
+
 }
