bafd30139e23cabc0394f3b74d77790dfa10b8e8
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-22 23:06:14 +0300

    add object EXTRA_STICKER

--- app/src/androidTest/java/com/bankmtk/neuromemory/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/bankmtk/neuromemory/ui/main/MainActivityTest.kt
@@ -1,6 +1,7 @@
 package com.bankmtk.neuromemory.ui.main
 
 import androidx.test.espresso.intent.rule.IntentsTestRule
+import com.bankmtk.neuromemory.ui.sticker.StickerActivity
 import org.junit.Assert.*
 import org.junit.Rule
 
@@ -8,4 +9,7 @@ class MainActivityTest{
 
     @get:Rule
     val activityTestRule = IntentsTestRule(MainActivity::class.java, true, false)
+
+    private val EXTRA_STICKER = StickerActivity::class.java.name+"extra.STICKER_ID"
+
 }
