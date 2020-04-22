8477b7b2412500f57760185144126dd3619a46eb
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-22 23:04:28 +0300

    add Rule

--- app/src/androidTest/java/com/bankmtk/neuromemory/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/bankmtk/neuromemory/ui/main/MainActivityTest.kt
@@ -1,7 +1,11 @@
 package com.bankmtk.neuromemory.ui.main
 
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import org.junit.Assert.*
+import org.junit.Rule
 
 class MainActivityTest{
 
+    @get:Rule
+    val activityTestRule = IntentsTestRule(MainActivity::class.java, true, false)
 }
