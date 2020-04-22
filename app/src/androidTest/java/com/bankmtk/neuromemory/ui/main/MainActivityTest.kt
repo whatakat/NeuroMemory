88802c10d0d6516df44d2d906fe0d67c43efe782
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-22 23:07:17 +0300

    add object viewModel

--- app/src/androidTest/java/com/bankmtk/neuromemory/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/bankmtk/neuromemory/ui/main/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.bankmtk.neuromemory.ui.main
 
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import com.bankmtk.neuromemory.ui.sticker.StickerActivity
+import io.mockk.mockk
 import org.junit.Assert.*
 import org.junit.Rule
 
@@ -11,5 +12,6 @@ class MainActivityTest{
     val activityTestRule = IntentsTestRule(MainActivity::class.java, true, false)
 
     private val EXTRA_STICKER = StickerActivity::class.java.name+"extra.STICKER_ID"
+    private val viewModel: MainViewModel = mockk(relaxed = true)
 
 }
