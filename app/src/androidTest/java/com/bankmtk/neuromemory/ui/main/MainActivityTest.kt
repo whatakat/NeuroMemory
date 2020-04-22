bbca8885c8ba82f108907e5a6f41d622e3bf5c81
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-22 23:12:01 +0300

    add object testStickers

--- app/src/androidTest/java/com/bankmtk/neuromemory/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/bankmtk/neuromemory/ui/main/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.bankmtk.neuromemory.ui.main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.test.espresso.intent.rule.IntentsTestRule
+import com.bankmtk.neuromemory.data.model.Sticker
 import com.bankmtk.neuromemory.ui.sticker.StickerActivity
 import io.mockk.mockk
 import org.junit.Assert.*
@@ -15,5 +16,7 @@ class MainActivityTest{
     private val EXTRA_STICKER = StickerActivity::class.java.name+"extra.STICKER_ID"
     private val viewModel: MainViewModel = mockk(relaxed = true)
     private val viewStateLiveData = MutableLiveData<MainViewState>()
-
+    private val testStickers = listOf(Sticker("333","first","langOne","langTwo"),
+        Sticker("444","second","langOne1","langTwo1"),
+        Sticker("555","third","langOne2","langTwo2"))
 }
