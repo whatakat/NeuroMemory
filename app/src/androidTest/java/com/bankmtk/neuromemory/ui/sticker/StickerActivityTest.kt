986e2d43f91b6c1cf9aca56a078a01d78e413c25
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-23 17:48:45 +0300

    fix fun should_show_color_picker()

--- app/src/androidTest/java/com/bankmtk/neuromemory/ui/sticker/StickerActivityTest.kt
+++ app/src/androidTest/java/com/bankmtk/neuromemory/ui/sticker/StickerActivityTest.kt
@@ -5,9 +5,9 @@ import android.graphics.drawable.ColorDrawable
 import androidx.lifecycle.MutableLiveData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
-import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import org.junit.Rule
 import androidx.test.rule.ActivityTestRule
@@ -15,8 +15,6 @@ import com.bankmtk.neuromemory.R
 import com.bankmtk.neuromemory.data.model.Color
 import com.bankmtk.neuromemory.data.model.Sticker
 import com.bankmtk.neuromemory.extentions.getColorInt
-import com.google.common.base.CharMatcher.`is`
-import com.google.common.base.Predicates.not
 import io.mockk.*
 import junit.framework.Assert.assertTrue
 import org.junit.After
@@ -25,7 +23,6 @@ import org.junit.Test
 import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 import org.koin.standalone.StandAloneContext.loadKoinModules
-import java.util.regex.Pattern.matches
 
 
 class StickerActivityTest{
@@ -55,12 +52,12 @@ class StickerActivityTest{
 
     private val testSticker = Sticker("333","title","langOne","langTwo")
 
-//    @Test
-//    fun should_show_color_picker(){
-//        onView(withId(R.id.palette)).perform(click())
-//
-//        onView(withId(R.id.colorPicker)).check(matches(isCompletelyDisplayed()))
-//    }
+    @Test
+    fun should_show_color_picker(){
+        onView(withId(R.id.palette)).perform(click())
+
+        onView(withId(R.id.colorPicker)).check(matches(isCompletelyDisplayed()))
+    }
 //    @Test
 //    fun should_hide_color_picker(){
 //        onView(withId(R.id.palette)).perform(click()).perform(click())
