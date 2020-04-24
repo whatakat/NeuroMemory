fe82fdd462ae4ccd9ad562aaecbdecbae9d696ea
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-24 20:55:03 +0300

    correct override suspend fun getStickerById(id: String): Sticker

--- app/src/main/java/com/bankmtk/neuromemory/data/provider/FireStoreProvider.kt
+++ app/src/main/java/com/bankmtk/neuromemory/data/provider/FireStoreProvider.kt
@@ -9,6 +9,9 @@ import com.bankmtk.neuromemory.data.model.Result
 import com.bankmtk.neuromemory.data.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.*
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 private const val STICKERS_COLLECTION = "stickers"
 private const val USERS_COLLECTION = "users"
@@ -41,20 +44,21 @@ class FireStoreProvider(private val firebaseAuth: FirebaseAuth, private val db:F
             }
         }
 
-    override fun getStickerById(id: String): LiveData<Result> =
-        MutableLiveData<Result>().apply {
+    override suspend fun getStickerById(id: String): Sticker =
+        suspendCoroutine { continuation ->
             try {
                 getUserStickersCollection().document(id).get()
                     .addOnSuccessListener {
-                        value =
-                            Result.Success(it.toObject(Sticker::class.java))
+                        continuation.resume(it.toObject(Sticker::class.java)!!)
                     }.addOnFailureListener {
-                        throw it
+                        continuation.resumeWithException(it)
                     }
-            }catch (e: Throwable){
-                value = Result.Error(e)
+            } catch (e: Throwable){
+                continuation.resumeWithException(e)
             }
         }
+
+
 
     override fun subscribeToAllStickers(): LiveData<Result> =
         MutableLiveData<Result>().apply {
