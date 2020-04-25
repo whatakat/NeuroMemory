48244819240040d937ef51f4e6e88531e8cd6348
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-25 12:26:05 +0300

    refactor subscribeToAllStickers

--- app/src/main/java/com/bankmtk/neuromemory/data/provider/RemoteDataProvider.kt
+++ app/src/main/java/com/bankmtk/neuromemory/data/provider/RemoteDataProvider.kt
@@ -1,12 +1,12 @@
 package com.bankmtk.neuromemory.data.provider
 
-import androidx.lifecycle.LiveData
 import com.bankmtk.neuromemory.data.model.Sticker
 import com.bankmtk.neuromemory.data.model.Result
 import com.bankmtk.neuromemory.data.model.User
+import kotlinx.coroutines.channels.ReceiveChannel
 
 interface RemoteDataProvider {
-    fun subscribeToAllStickers(): LiveData<Result>
+    fun subscribeToAllStickers(): ReceiveChannel<Result>
     suspend fun getStickerById(id: String): Sticker
     suspend fun saveSticker(sticker: Sticker): Sticker
     suspend fun getCurrentUser() : User?
