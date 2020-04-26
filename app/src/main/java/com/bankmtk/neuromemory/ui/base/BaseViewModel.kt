83eb517fa6259adb5664fedcedccc7ef2e71bd89
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-26 20:40:08 +0300

    refactor BaseViewModel add override fun onCleared()

--- app/src/main/java/com/bankmtk/neuromemory/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/base/BaseViewModel.kt
@@ -1,13 +1,10 @@
 package com.bankmtk.neuromemory.ui.base
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 open class BaseViewModel<S>: ViewModel(), CoroutineScope {
@@ -29,5 +26,10 @@ open class BaseViewModel<S>: ViewModel(), CoroutineScope {
             viewStateChannel.send(data)
         }
     }
-
+    override fun onCleared() {
+        viewStateChannel.close()
+        errorChannel.close()
+        coroutineContext.cancel()
+        super.onCleared()
+    }
 }
