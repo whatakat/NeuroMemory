abd9da69a99fdcbe5f8f3aaf9354b9d7e73de3c7
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-26 20:31:33 +0300

    refactor BaseViewModel add viewStateChannel

--- app/src/main/java/com/bankmtk/neuromemory/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/base/BaseViewModel.kt
@@ -4,11 +4,14 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.Channel
 import kotlin.coroutines.CoroutineContext
 
 open class BaseViewModel<S>: ViewModel(), CoroutineScope {
     override val coroutineContext: CoroutineContext by lazy {
         Dispatchers.Default + Job()
     }
+    private val viewStateChannel = BroadcastChannel<S>(Channel.CONFLATED)
 
 }
