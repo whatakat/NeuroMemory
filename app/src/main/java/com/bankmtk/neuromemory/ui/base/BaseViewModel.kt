cda33acdc0efa4b9aaa3c6d025c14cc6757eec9a
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-26 20:29:37 +0300

    refactor BaseViewModel override val

--- app/src/main/java/com/bankmtk/neuromemory/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/base/BaseViewModel.kt
@@ -1,10 +1,14 @@
 package com.bankmtk.neuromemory.ui.base
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
 
-open class BaseViewModel<T, S : BaseViewState<T>>: ViewModel() {
-    open val viewStateLiveData = MutableLiveData<S>()
-    open fun getViewState(): LiveData<S> = viewStateLiveData
+open class BaseViewModel<S>: ViewModel(), CoroutineScope {
+    override val coroutineContext: CoroutineContext by lazy {
+        Dispatchers.Default + Job()
+    }
+
 }
