6d30d846b956805abdec0e48bb8d25278c2fd891
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-27 15:53:01 +0300

    refactor SplashActivity

--- app/src/main/java/com/bankmtk/neuromemory/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/splash/SplashViewModel.kt
@@ -5,8 +5,8 @@ import com.bankmtk.neuromemory.data.errors.NoAuthException
 import com.bankmtk.neuromemory.ui.base.BaseViewModel
 import kotlinx.coroutines.launch
 
-class SplashViewModel(private val repository: Repository):
-BaseViewModel<Boolean>(){
+class SplashViewModel(val repository: Repository):
+BaseViewModel<Boolean?>(){
     fun requestUser(){
         launch {
             repository.getCurrentUser()?.let {
