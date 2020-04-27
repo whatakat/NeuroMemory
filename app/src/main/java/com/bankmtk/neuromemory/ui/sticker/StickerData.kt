ab66d5d4746b6004159a4de91835531164e2fe6a
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-04-27 15:08:43 +0300

    add StickerData

---
+++ app/src/main/java/com/bankmtk/neuromemory/ui/sticker/StickerData.kt
@@ -0,0 +1,5 @@
+package com.bankmtk.neuromemory.ui.sticker
+
+import com.bankmtk.neuromemory.data.model.Sticker
+
+data class StickerData (val isDeleted: Boolean = false, val stocker: Sticker? = null)
