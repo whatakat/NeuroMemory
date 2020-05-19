d645b440394e8d12488f05a340b8dfea08a2d27d
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-05-19 22:18:40 +0300

    refactor

--- app/src/main/java/com/bankmtk/neuromemory/ui/main/MainAdapter.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/main/MainAdapter.kt
@@ -3,6 +3,7 @@ package com.bankmtk.neuromemory.ui.main
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
