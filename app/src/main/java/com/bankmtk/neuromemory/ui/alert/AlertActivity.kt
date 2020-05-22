d363370f56aac6542b7d57f9d060026526317dd6
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-05-22 23:21:08 +0300

    refactor

--- app/src/main/java/com/bankmtk/neuromemory/ui/alert/AlertActivity.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/alert/AlertActivity.kt
@@ -1,4 +1,64 @@
 package com.bankmtk.neuromemory.ui.alert
 
-class AlertActivity {
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import com.bankmtk.neuromemory.R
+import com.bankmtk.neuromemory.data.model.Sticker
+import com.bankmtk.neuromemory.ui.base.BaseActivity
+import com.bankmtk.neuromemory.ui.main.MainViewModel
+import kotlinx.android.synthetic.main.activity_alert.*
+import kotlinx.android.synthetic.main.activity_main.myRecycler
+import kotlinx.android.synthetic.main.activity_main.toolbar
+import kotlinx.android.synthetic.main.item_sticker.view.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.koin.android.viewmodel.ext.android.viewModel
+
+class AlertActivity:BaseActivity<List<Sticker>?>() {
+
+    @ExperimentalCoroutinesApi
+    override val model: MainViewModel by viewModel()
+    override val layoutRes: Int= R.layout.activity_alert
+    private lateinit var adapter: AlertAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setSupportActionBar(toolbar)
+        overridePendingTransition(R.anim.slidein, R.anim.slideout)
+
+
+        adapter = AlertAdapter(object : AlertAdapter.OnItemClickListener{
+
+            @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+            override fun onItemClick(itemView: View) {
+                if (itemView.langOneI.visibility == View.VISIBLE){
+                    animateView(itemView)
+                }else{
+                    animateViewCancel(itemView)
+                }
+            }
+        })
+        myRecycler.adapter = adapter
+
+        fabOk.setOnClickListener { deleteStickerScreen(null) }
+    }
+
+    override fun renderData(data: List<Sticker>?) {
+        if (data==null) return
+        adapter.stickers = data
+    }
+    private fun deleteStickerScreen(sticker: Sticker?){
+    }
+    companion object{
+        fun getStartIntent(context: Context) = Intent(context,
+            AlertActivity::class.java)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        overridePendingTransition(R.anim.slideout, R.anim.slidein)
+    }
 }
--- app/src/main/java/com/bankmtk/neuromemory/ui/alert/AlertAdapter.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/alert/AlertAdapter.kt
@@ -1,4 +1,47 @@
 package com.bankmtk.neuromemory.ui.alert
 
-class AlertAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bankmtk.neuromemory.R
+import com.bankmtk.neuromemory.data.model.Sticker
+import com.bankmtk.neuromemory.extentions.getColorInt
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_sticker.*
+
+class AlertAdapter(private val onItemClickListener: OnItemClickListener)
+    : RecyclerView.Adapter<AlertAdapter.StickViewHolder>() {
+    var stickers: List<Sticker> = listOf()
+        set(value){
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StickViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view  = inflater.inflate(R.layout.item_sticker,parent,false)
+        return StickViewHolder(view)
+    }
+
+    override fun getItemCount() = stickers.size
+
+
+    override fun onBindViewHolder(holder: StickViewHolder, position: Int){
+        holder.bind(stickers[position])
+    }
+    inner class StickViewHolder(override val containerView: View):
+        RecyclerView.ViewHolder(containerView), LayoutContainer {
+        fun bind(sticker: Sticker){
+            titleStick.text = sticker.title
+            langOneI.text = sticker.langOne
+            langTwoI.text = sticker.langTwo
+            itemView.setBackgroundColor(sticker.color.getColorInt(itemView.context))
+            itemView.setOnClickListener{onItemClickListener.onItemClick(itemView)}
+        }
+    }
+    interface OnItemClickListener{
+        fun onItemClick(itemView: View)
+    }
+
 }
