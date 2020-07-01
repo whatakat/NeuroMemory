5eea1ac915721c3690bef86c93297ab3062e1e95
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-07-01 22:31:11 +0300

    refactor

--- app/src/main/java/com/bankmtk/neuromemory/ui/main/MainActivity.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/main/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : BaseActivity<List<Sticker>?>() {
     lateinit var builder: Notification.Builder
     private val channelId = "com.bankmtk.neuromemory.service"
     private val description = "Test notification"
-    private val vibrate = longArrayOf(0,50,100,50,50,50)
+    //private val vibrate = longArrayOf(0,50,100,50,50,50)
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -151,8 +151,8 @@ class MainActivity : BaseActivity<List<Sticker>?>() {
             notificationChannel = NotificationChannel(channelId,description, NotificationManager.IMPORTANCE_HIGH)
             notificationChannel.enableLights(true)
             notificationChannel.lightColor = Color.GREEN
-            notificationChannel.enableVibration(true)
-            notificationChannel.vibrationPattern = vibrate
+//            notificationChannel.enableVibration(true)
+//            notificationChannel.vibrationPattern = vibrate
             notificationManager.createNotificationChannel(notificationChannel)
 
             builder = Notification.Builder(this,channelId)
