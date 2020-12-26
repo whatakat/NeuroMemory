7983b9f5a8a8b4371734af8e62530535721b5ec3
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-12-26 23:52:42 +0300

    delete import

--- app/src/main/java/com/bankmtk/neuromemory/ui/main/MainAdapter.kt
+++ app/src/main/java/com/bankmtk/neuromemory/ui/main/MainAdapter.kt
@@ -6,7 +6,6 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
 import com.bankmtk.neuromemory.R
 import com.bankmtk.neuromemory.data.model.Sticker
